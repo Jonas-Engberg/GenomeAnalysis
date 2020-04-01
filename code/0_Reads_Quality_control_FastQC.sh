9f8995e8b47cdf1b6ba8a9c5988a711eb832b608
Jonas-Engberg <jonashengberg@gmail.com>
2020-04-01 14:31:15 +0200

    added 0_Reads_quality_control_FastQC.sh version

--- code/0_Reads_Quality_control_FastQC.sh
+++ code/0_Reads_Quality_control_FastQC.sh
@@ -4,14 +4,14 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 00:00:00
-#SBATCH -J job_name
+#SBATCH -J Preprocessing_of_Data_JE
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user your_email
+#SBATCH --mail-user earthius@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load ....
+module load FastQC/0.11.8
 
 # Your commands
-<Command_1...>
-<Command_2...>
+fastqc -f fastq -t 2 -o /home/joen8526/GenomeAnalysis/analyses/01_preprocessing/DNA /home/joen8526/GenomeAnalysis/raw_data/DNA_trimmed/*.fastq.gz
+fastqc -f fastq -t 2 -o /home/joen8526/GenomeAnalysis/analyses/01_preprocessing/RNA /home/joen8526/GenomeAnalysis/raw_data/RNA_untrimmed/*.fastq.gz
