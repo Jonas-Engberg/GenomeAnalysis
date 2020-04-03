908f6051ebf9ecd0d960e3490b7f7b08ae091c3d
Jonas Engberg <joen8526@rackham2.uppmax.uu.se>
2020-04-03 17:22:35 +0200

    Trimmed and QC:ed RNA data

---
+++ code/0_Reads_Quality_Control_RNA_trimmed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:15:00
+#SBATCH -J Preprocessing_of_data_FastQC_JE
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user earthius@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load FastQC/0.11.8
+
+# Your commands
+fastqc -f fastq -t 2 -o /home/joen8526/GenomeAnalysis/analyses/01_preprocessing/RNA_trimmed /home/joen8526/GenomeAnalysis/data/trimmed_data/*.fastq.gz
--- code/1_trimming.sh
+++ code/1_trimming.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:40:00
+#SBATCH -t 00:10:00
 #SBATCH -J Trimmomatic_Run_JE
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user earthius@gmail.com
@@ -18,19 +18,19 @@ module load trimmomatic
 # Since Illumina 1.8+ the standard phred score is -phred33
 
 trimmomatic PE -threads 2 -phred33 -trimlog /home/joen8526/GenomeAnalysis/data/trimmed_data/trimlog_SRR4342137 \
-/home/joen8526/GenomeAnalysis/raw_data/RNA_untrimmed/SRR4342137.1.fastq.gz \
-/home/joen8526/GenomeAnalysis/raw_data/RNA_untrimmed/SRR4342137.2.fastq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Paired_Forward.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Unpaired_Forward.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Paired_Reversed.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Unpaired_Reversed.fq.gz \
+/home/joen8526/GenomeAnalysis/data/raw_data/RNA_untrimmed/SRR4342137.1.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/raw_data/RNA_untrimmed/SRR4342137.2.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Paired_Forward.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Unpaired_Forward.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Paired_Reversed.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342137_Unpaired_Reversed.fastq.gz \
 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
 
 trimmomatic PE -threads 2 -phred33 -trimlog /home/joen8526/GenomeAnalysis/data/trimmed_data/trimlog_SRR4342139 \
-/home/joen8526/GenomeAnalysis/raw_data/RNA_untrimmed/SRR4342139.1.fastq.gz \
-/home/joen8526/GenomeAnalysis/raw_data/RNA_untrimmed/SRR4342139.2.fastq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Paired_Forward.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Unpaired_Forward.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Paired_Reversed.fq.gz \
-/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Unpaired_Reversed.fq.gz \
+/home/joen8526/GenomeAnalysis/data/raw_data/RNA_untrimmed/SRR4342139.1.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/raw_data/RNA_untrimmed/SRR4342139.2.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Paired_Forward.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Unpaired_Forward.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Paired_Reversed.fastq.gz \
+/home/joen8526/GenomeAnalysis/data/trimmed_data/SRR4342139_Unpaired_Reversed.fastq.gz \
 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
