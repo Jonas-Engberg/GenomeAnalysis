10e13dc5ffef8427e41f6709e8bfb8245cea7540
Jonas Engberg <joen8526@rackham3.uppmax.uu.se>
2020-04-16 10:00:27 +0200

    Moved raw_data into data folder and updated 2_Assembly_Megahit.sh

--- code/2_Assembly_Megahit.sh
+++ code/2_Assembly_Megahit.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 7:30:00
+#SBATCH -t 5:30:00
 #SBATCH -J Megahit_JonasEngberg
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user earthius@gmail.com
@@ -15,13 +15,13 @@ module load megahit
 #Commands
 # From Zone D1
 megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass -t 2 \
--1 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,\
--2 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,\
+-1 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz \
+-2 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz \
 -o /home/joen8526/GenomeAnalysis/data/assembled_genome_data/Site_D1
 
 # From Zone D3
-megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass -t 2 \
--1 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz \
--2 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz \
--o /home/joen8526/GenomeAnalysis/data/assembled_genome_data/Site_D3
+#megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass -t 2 \
+#-1 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz \
+#-2 /home/joen8526/GenomeAnalysis/data/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz \
+#-o /home/joen8526/GenomeAnalysis/data/assembled_genome_data/Site_D3
 
