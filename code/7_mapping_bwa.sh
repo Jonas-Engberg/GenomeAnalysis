707a14ff550e92ee637b6b296319dbe108c04ab5
Jonas-Engberg <jonashengberg@gmail.com>
2020-05-07 11:50:29 +0200

    Wrote mapping script

---
+++ code/7_mapping_bwa.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:00:00
+#SBATCH -J BWA Thrash#SBATCH --mail-type=ALL
+#SBATCH --mail-user earthius@gmail.com
+
+# load modules
+module load bioinfo-tools
+module load bwa/0.7.17
+module load samtools/1.10
+
+# My commands
+
+for bin in /home/joen8526/GenomeAnalysis/data/binned_metagenome/*.fa
+
+do
+  
+  bwa index $bin
+  binnum=${bin/#$"/home/joen8526/GenomeAnalysis/data/binned_metagenome/"}
+
+  # 
+  bwa mem -t 2 $bin ~/GenomeAnalysis/data/trimmed_data_2/SRR4342137_Paired_Forward.fastq.gz \
+  ~/GenomeAnalysis/data/trimmed_data_2/SRR4342137_Paired_Reversed.fastq.gz \
+  > ~/GenomeAnalysis/data/mapped_RNA/SRR4342137/${binnum}_mapped_SRR4342137.sam
+  
+  bwa mem -t 2 $bin ~/GenomeAnalysis/data/trimmed_data_2/SRR4342139_Paired_Forward.fastq.gz \
+  ~/GenomeAnalysis/data/trimmed_data_2/SRR4342139_Paired_Reversed.fastq.gz \
+  > ~/GenomeAnalysis/data/mapped_RNA/SRR4342139/${binnum}_mapped_SRR4342139.sam
+
+  #Converting -sam file to sorted .bam-file using samtools piping
+  samtools view -b ~/GenomeAnalysis/data/mapped_RNA/SRR4342137/${binnum}_mapped_SRR4342137.sam | samtools sort - -o ~/GenomeAnalysis/data/mapped_RNA/SRR4342137/${binnum}_mapped_SRR4342137.bam
+  samtools view -b ~/GenomeAnalysis/data/mapped_RNA/SRR4342139/${binnum}_mapped_SRR4342139.sam | samtools sort - -o ~/GenomeAnalysis/data/mapped_RNA/SRR4342139/${binnum}_mapped_SRR4342139.bam
+
+  #Removing the sam files
+  rm ~/GenomeAnalysis/data/mapped_RNA/SRR4342137/${binnum}_mapped_SRR4342137.sam
+  rm ~/GenomeAnalysis/data/mapped_RNA/SRR4342139/${binnum}_mapped_SRR4342139.sam
+
+done
+
+
